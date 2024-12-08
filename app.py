b9e53c6355d12eca749a718e94cef59d5179e575
murthy-oss <murthysuggested1@gmail.com>
2024-12-08 22:53:18 +0600

    Add flask_cors to requirements

--- app.py
+++ app.py
@@ -3,24 +3,30 @@ from flask_cors import CORS
 
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 from config import *
 
 app = Flask(__name__)
 CORS(app)
 
-client = MongoClient(MONGODB_CONNECTION_STRING)
+client = MongoClient(os.getenv("MONGODB_CONNECTION_STRING"))
 database = client[MONGODB_DATABASE_NAME]
 items_collection = database[MONGODB_TODO_ITEMS_COLLECTION]
 
 @app.get("/get_todo_items")
 def get_todo_items():
-    items_list = items_collection.find()
-    items_list = [{"id": str(item["_id"]), "content": item["content"]} for item in items_list]
-
-    return jsonify({
+    try:
+        items_list = items_collection.find()
+        items_list = [{"id": str(item["_id"]), "content": item["content"]} for item in items_list]
+        return jsonify({
         "todoItems": items_list
-    })
+        })
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 @app.route("/")
 def home():
     return "Welcome to the app!"
@@ -57,3 +63,4 @@ def update_todo_item():
         return jsonify({"message": "Todo item updated successfully"})
     else:
         return jsonify({"message": "Todo item not found or no change made"}), 404
+
--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-MONGODB_CONNECTION_STRING = "mongodb+srv://vinaylakkoju17:OujzXgTkJNDDyNQo@cluster0.44o1j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 MONGODB_DATABASE_NAME = "Tododatabase"
 MONGODB_TODO_ITEMS_COLLECTION = "todoItems"
