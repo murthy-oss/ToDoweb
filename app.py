3800e30b704572527168b250e5dfb0dd8992c2a5
murthy-oss <murthysuggested1@gmail.com>
2024-12-08 23:30:12 +0600

    7th commit

--- app.py
+++ app.py
@@ -3,33 +3,24 @@ from flask_cors import CORS
 
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 from config import *
 
 app = Flask(__name__)
 CORS(app)
 
-client = MongoClient(os.getenv("MONGO_URI"),tls=True, tlsAllowInvalidCertificates=True)
+client = MongoClient(MONGODB_CONNECTION_STRING)
 database = client[MONGODB_DATABASE_NAME]
 items_collection = database[MONGODB_TODO_ITEMS_COLLECTION]
 
 @app.get("/get_todo_items")
 def get_todo_items():
-    try:
-        items_list = items_collection.find()
-        items_list = [{"id": str(item["_id"]), "content": item["content"]} for item in items_list]
-        return jsonify({
+    items_list = items_collection.find()
+    items_list = [{"id": str(item["_id"]), "content": item["content"]} for item in items_list]
+
+    return jsonify({
         "todoItems": items_list
-        })
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-@app.route("/")
-def home():
-    return "Welcome to the app!"
+    })
 
 
 @app.post("/add_todo_item")
@@ -63,4 +54,3 @@ def update_todo_item():
         return jsonify({"message": "Todo item updated successfully"})
     else:
         return jsonify({"message": "Todo item not found or no change made"}), 404
-
